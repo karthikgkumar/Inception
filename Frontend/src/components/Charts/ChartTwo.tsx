729f957ee254260284865407509a289afed454e3
amilpa <amilpa2020@gmail.com>
2024-03-24 03:29:51 +0530

    bug fixes

--- Frontend/src/components/Charts/ChartTwo.tsx
+++ Frontend/src/components/Charts/ChartTwo.tsx
@@ -73,11 +73,11 @@ const ChartTwo: React.FC = () => {
   const [state, setState] = useState<ChartTwoState>({
     series: [
       {
-        name: "Memories",
+        name: "Recalls",
         data: [44, 55, 41, 67, 22, 43, 65],
       },
       {
-        name: "Recalls",
+        name: "Memories",
         data: [13, 23, 20, 8, 13, 27, 15],
       },
     ],
